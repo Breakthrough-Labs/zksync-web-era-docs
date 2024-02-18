ce00786e90c50259cc946dc48b101eae0c9a546d
Anton Dosta <a.dosta@icloud.com>
2024-02-18 02:39:42 -0800

    ci: trying to make vercel work

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -27,6 +27,7 @@ export default defineUserConfig({
     ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
     ["link", { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: true }],
     ["link", { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" }],
+    // ["link", { rel: "stylesheet", href: "https://cookbook-docsbot-staging.vercel.app/docsbot.css" }],
     [
       "meta",
       {
@@ -102,6 +103,19 @@ export default defineUserConfig({
         let contributorsDiv = document.querySelector('.contributors');
         contributorsDiv.innerHTML = '<span class="label">Contributors: </span>' + updatedList;
       
+        const cookbookStyles = document.createElement('link');
+        cookbookStyles.rel = 'stylesheet';
+        cookbookStyles.href = 'https://cookbook-docsbot-staging.vercel.app/docsbot.css';
+        document.head.prepend(cookbookStyles);
+
+        const cookbookContainer = document.createElement('div');
+        cookbookContainer.id = '__cookbook';
+        cookbookContainer.setAttribute('data-api-key', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWNlNzUwZmZiMDBlZWUyNThjNmUxMjkiLCJpYXQiOjE3MDgwMjkxOTksImV4cCI6MjAyMzYwNTE5OX0.vrpJUZNG2jBFegOyENxgLJfStfyP7R1sQYE_I4XNo40");
+        document.body.appendChild(cookbookContainer);
+
+        const cookbookScript = document.createElement('script');
+        cookbookScript.src = 'https://cookbook-docsbot-staging.vercel.app/docsbot.min.js';
+        document.head.appendChild(cookbookScript);
       });
 
       `,
